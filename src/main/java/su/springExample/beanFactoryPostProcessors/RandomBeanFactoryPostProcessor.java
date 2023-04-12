bacbfbacc28e5e2bbc4ba909d43b719eabe36823
FadeIn64 <danik6052@gmail.com>
2023-04-12 17:39:23 +0400

    try to register prototype

--- src/main/java/su/springExample/beanFactoryPostProcessors/RandomBeanFactoryPostProcessor.java
+++ src/main/java/su/springExample/beanFactoryPostProcessors/RandomBeanFactoryPostProcessor.java
@@ -1,15 +1,17 @@
 package su.springExample.beanFactoryPostProcessors;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.config.BeanDefinitionCustomizer;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
 @Component
-public class RandomBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
-    @Override
+public class RandomBeanFactoryPostProcessor implements FactoryBean<Integer> {
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         if(beanFactory.containsBean("random")) {
             System.out.println("yes");
@@ -18,6 +20,21 @@ public class RandomBeanFactoryPostProcessor implements BeanFactoryPostProcessor 
 
         beanFactory.registerSingleton("random", (new Random()).nextInt(0,99));
 
+    }
+
+    @Override
+    public Integer getObject() throws Exception {
+        return (new Random()).nextInt(0, 99);
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return Integer.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
     }
 }
 
