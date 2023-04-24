e7124aec10b7021f101bdd4106c1b3462a9a31dc
FadeIn64 <danik6052@gmail.com>
2023-04-24 19:20:55 +0400

    ADD TO ORM CHMONIMORPHISM

--- src/main/java/su/orm/DBExtractor.java
+++ src/main/java/su/orm/DBExtractor.java
@@ -3,17 +3,12 @@ package su.orm;
 
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
-import su.reflection.FieldCollector;
 
-import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.Statement;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @Component
 public class DBExtractor {
@@ -31,14 +26,10 @@ public class DBExtractor {
     @SneakyThrows
     public <T> List<T> getList(Class<T> clazz){
 
-        List<T> res = new ArrayList<>();
-
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(sqlStringBuilder.extractQuery(clazz));
 
-        res = convertor.convert(resultSet, clazz);
-
-        return res;
+        return convertor.convert(resultSet, clazz);
 
     }
 
